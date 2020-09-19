f7efa0167c841d3000f173cffa7411bede4956ba
deiverhs <deiverhs@outlook.com>
2020-09-19 14:59:43 -0600

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "962448.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': true,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 34945001, 34728290, 34728777, 34310764, 34728195, 33571860, 33569697, 33570277, 34751966, 34757277, 34762761, 34764437, 34777051, 35000840, 35027804, 35143340, 35187940, 35193697, 35195835, 35230984, 35275060, 35276891, 35289040, 35462112 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/33617016/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/33617017/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/33617015/1/ammo.js', 'preload' : true},
+];
